7e6e95ed3818449d4b1a94c762d53f65628fbba3
Nikosane <niteshkotian3@gmai.com>
2023-10-26 21:30:58 +0530

    location update

--- youtube_videoDownloader.py
+++ youtube_videoDownloader.py
@@ -1,5 +1,13 @@
-# new 
-# hello
-# new update 
-print("hello world!")
-# second change 
+from pytube import YouTube
+import sys 
+
+link = sys.argv[1]
+
+yt = YouTube(link)
+
+print("Title : ", yt.title)
+print("Views: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download('D:\Downloads\Video')
