ca08e8a324ede77097976f8a352b4e4339036c6c
Nikosane <niteshkotian3@gmai.com>
2023-10-27 20:27:55 +0530

    app

--- youtube_videoDownloader.py
+++ youtube_videoDownloader.py
@@ -1,13 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
 from pytube import YouTube
-import sys 
 
-link = sys.argv[1]
+# Function to download the video
+def download_video():
+    url = url_entry.get()
+    try:
+        yt = YouTube(url)
+        video_stream = yt.streams.get_highest_resolution()
+        download_path = destination_entry.get()
+        video_stream.download(download_path)
+        messagebox.showinfo("Download Complete", "Video downloaded successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
-yt = YouTube(link)
+# Create the main window
+window = tk.Tk()
+window.title("YouTube Video Downloader")
 
-print("Title : ", yt.title)
-print("Views: ", yt.views)
+# Create and pack labels and entry fields
+url_label = tk.Label(window, text="Enter YouTube URL:")
+url_label.pack()
+url_entry = tk.Entry(window, width=50)
+url_entry.pack()
 
-yd = yt.streams.get_highest_resolution()
+destination_label = tk.Label(window, text="Destination Folder:")
+destination_label.pack()
+destination_entry = tk.Entry(window, width=50)
+destination_entry.pack()
 
-yd.download('D:\Downloads\Video')
+# Create and pack download button
+download_button = tk.Button(window, text="Download Video", command=download_video)
+download_button.pack()
+
+# Run the main loop
+window.mainloop()
